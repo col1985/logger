ceab54a684b035a955ced1ee4f2a1292a6f5c093
CookieMonster <col1985@gmail.com>
2015-02-10 19:00:13 +0000

    update of logger implenentation - ongoing

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-module.exports = function() {
+'use strict';
 
-    'use strict';
-
-    var logger = require('winston');
-
-    //set logger output levels
-    logger.setLevels({
-        debug: 0,
-        info: 1,
-        silly: 2,
-        warn: 3,
-        error: 4
-    });
-
-    // set logger out colors
-    logger.addColors({
-        debug: 'green',
-        info: 'cyan',
-        silly: 'magenta',
-        warn: 'yellow',
-        error: 'red'
-    });
-
-    // remove old transport defintion
-    logger.remove(logger.transports.Console);
-
-    //set new custom logger definition
-    logger.add(logger.transports.Console, {
-        level: 'debug',
-        colorize: true,
-        handleExceptions: true,
-        json: false,
-        timestamp: function() {
-            return new Date().toUTCString();
+module.exports = {
+    getLogger: function () {
+        return Logger.log
+    },
+    setLogLevel: function (level) {
+        if (!level) {
+            return 'debug';
         }
-    });
+        return level;
+    },
+    enableJson: function () {
 
-    // retunr custom logger
-    return logger.log;
+    }
 };
+
+
+var winston = require('winston'),
+    Logger = new winston(),
+    date = new Date();
+
+// remove old transport defintion
+Logger.remove(winston.transports.Console);
+
+// //set logger output levels
+// logger.setLevels({
+//     debug: 0,
+//     info: 1,
+//     silly: 2,
+//     warn: 3,
+//     error: 4
+// });
+
+// // set logger out colors
+// logger.addColors({
+//     debug: 'green',
+//     info: 'cyan',
+//     silly: 'magenta',
+//     warn: 'yellow',
+//     error: 'red'
+// });
+
+
+//set new custom logger definition
+logger.add(logger.transports.Console, {
+    level: 'debug',
+    colorize: true,
+    handleExceptions: true,
+    json: false,
+    timestamp: function () {
+        return date;
+    }
+});

648d428755082c24db144ff1ef08ce6eedf8fe15
Colum Bennett <colum.bennett@feedhenry.com>
2015-06-15 11:29:03 +0100

    updated logger logic to handle feedenry deployed environments, chnage output level based on optput

--- test/test.js
+++ test/test.js
@@ -1,84 +1,85 @@
 describe('myLogger module', function () {
 
-    var Logger = require('./../lib/Logger'),
-        myLogger = Logger.getLogger();
+  var Logger = require('./../lib/Logger'),
+    myLogger = Logger.getLogger(),
+    _ = require('lodash');
 
-    // we use child process to excute parser via CLI
-    it('should return an instance of the Logger module', function (done) {
-        if (Logger) {
-            done();
-        } else {
-            return done('Failed to instantciate Logger');
-        }
-    });
+  // we use child process to excute parser via CLI
+  it('should return an instance of the Logger module', function (done) {
+    if (Logger) {
+      done();
+    } else {
+      return done('Failed to instantciate Logger');
+    }
+  });
 
-    // we use child process to excute parser via CLI
-    it('should return an instance of the the custom Logger', function (done) {
-        if (myLogger) {
-            done();
-        } else {
-            return done('Failed to instantciate myLogger');
-        }
-    });
+  // we use child process to excute parser via CLI
+  it('should return an instance of the the custom Logger', function (done) {
+    if (myLogger) {
+      done();
+    } else {
+      return done('Failed to instantciate stdout-logger');
+    }
+  });
 
-    it('should return a colorized output for log level', function (done) {
-        if (myLogger) {
+  it('should return a colorized output for log level', function (done) {
+    if (myLogger) {
 
-            console.log('\n\n');
-            myLogger('debug', 'test debug level');
-            myLogger('info', 'test info level');
-            myLogger('silly', 'test silly level');
-            myLogger('warn', 'test warn level');
-            myLogger('error', 'test error level');
-            console.log('\n');
+      console.log('\n\n');
+      myLogger('debug', 'test debug level');
+      myLogger('info', 'test info level');
+      myLogger('silly', 'test silly level');
+      myLogger('warn', 'test warn level');
+      myLogger('error', 'test error level');
+      console.log('\n');
 
-            done();
-        } else {
-            return done('Failed to instantciate myLogger');
-        }
-    });
+      done();
+    } else {
+      return done('Failed to instantciate stdout-logger');
+    }
+  });
 
-    it('should turn off colorized output to console', function (done) {
-        if (Logger) {
-            Logger.set('colorize', false);
-            if (Logger.get('colorize') === false) {
-                done();
-            }
-        } else {
-            return done('Failed to instantciate myLogger');
-        }
-    });
+  it('should turn off colorized output to console', function (done) {
+    if (Logger) {
+      Logger.set('colorize', false);
+      if (Logger.get('colorize') === false) {
+        done();
+      }
+    } else {
+      return done('Failed to instantciate stdout-logger');
+    }
+  });
 
-    it('should silence log output to console', function (done) {
-        if (Logger) {
-            Logger.set('silent', true);
-            if (Logger.get('silent') === true) {
-                done();
-            }
-        } else {
-            return done('Failed to instantciate myLogger');
-        }
-    });
+  it('should silence log output to console', function (done) {
+    if (Logger) {
+      Logger.set('silent', true);
+      if (Logger.get('silent') === true) {
+        done();
+      }
+    } else {
+      return done('Failed to instantciate stdout-logger');
+    }
+  });
 
-    it('should turn on json format output to console\n', function (done) {
-        if (Logger) {
-            Logger.set('json', true);
-            if (Logger.get('json') === true) {
-                done();
-            }
-        } else {
-            return done('Failed to instantciate myLogger');
-        }
-    });
+  it('should turn on json format output to console\n', function (done) {
+    if (Logger) {
+      Logger.set('json', true);
+      if (Logger.get('json') === true) {
+        done();
+      }
+    } else {
+      return done('Failed to instantciate stdout-logger');
+    }
+  });
 
-    it('should not handle exceptions', function (done) {
-        if (Logger) {
-            Logger.set('handleExceptions', false);
-            if (Logger.get('handleExceptions') === false) {
-                done();
-            }
-        } else {
-            return done('Failed to instantciate myLogger');
-        }
-    });
+  it('should not handle exceptions', function (done) {
+    if (Logger) {
+      Logger.set('handleExceptions', false);
+      if (Logger.get('handleExceptions') === false) {
+        done();
+      }
+    } else {
+      return done('Failed to instantciate stdout-logger');
+    }
+  });
 });
